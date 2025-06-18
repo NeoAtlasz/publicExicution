e070db2f6a44014e38b83335555c9fb908e18258
NeoAtlasz <y0shii103288@gmail.com>
2025-06-18 04:26:01 +0000

    meow

---
+++ script.js
@@ -0,0 +1,30 @@
+addEventListener('DOMContentLoaded', () => {
+const btn = document.createElement('btn');
+btn.textContent = 'Add Item';
+document.body.appendChild(btn);
+
+let i = 0;
+btn.addEventListener('click', () => {
+    console.log('Button clicked');
+    let entry = document.createElement('li');
+entry.textContent = i;
+i++;
+list.appendChild(entry);
+entry.addEventListener('click', () => {
+    console.log('Item clicked', entryContent);
+    entry.removeChild();
+
+});
+
+});
+
+const list = document.createElement('ul');
+document.body.appendChild(list);
+
+const entry = document.createElement('li');
+entry.textContent = 'New Item';
+list.appendChild(entry);
+
+});
+
+
